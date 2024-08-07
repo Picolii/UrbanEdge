a431e44d2bdb50e39832c44abcf4aa941d163988
Picolii <bernardo.p2106@gmail.com>
2024-08-07 20:54:35 -0300

    starting from scratch

---
+++ projects/urban/src/lib/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+
+export class ButtonComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../../projects/urban/src/lib/header/header.component';
+import { ButtonComponent } from '../../projects/urban/src/lib/button/button.component';
+import { DxButtonModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
